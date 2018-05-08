0fb283136442fd97f31d26d71b44b4260a4482ec
AndrewKyu <andrewkyu95@gmail.com>
2018-05-08 14:02:55 -0700

    Google sign-in button allows google authentication

---
+++ app/src/main/java/com/example/andrewkyu/mytube/MyTubeHome.java
@@ -0,0 +1,13 @@
+package com.example.andrewkyu.mytube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTubeHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_tube_home);
+    }
+}
