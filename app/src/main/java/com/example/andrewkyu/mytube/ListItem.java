da053de6f3bf0f36df972ddb785c880490355688
AndrewKyu <andrewkyu95@gmail.com>
2018-05-12 13:48:06 -0700

    added listitems class and xml

---
+++ app/src/main/java/com/example/andrewkyu/mytube/ListItem.java
@@ -0,0 +1,20 @@
+package com.example.andrewkyu.mytube;
+
+public class ListItem {
+    private String head;
+    private String descrption;
+
+
+    public ListItem(String head, String descrption) {
+        this.head = head;
+        this.descrption = descrption;
+    }
+
+    public String getHead() {
+        return head;
+    }
+
+    public String getDescrption() {
+        return descrption;
+    }
+}
